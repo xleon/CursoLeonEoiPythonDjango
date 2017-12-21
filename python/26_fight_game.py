ec5c0eda353a8e443e157f35ef725072b741c298
Diego <malandro@gmail.com>
2017-12-21 22:33:58 +0100

    juego de lucha terminado

--- python/26_fight_game.py
+++ python/26_fight_game.py
@@ -1,6 +1,8 @@
 from colorama import init, Fore, Back, Style
 from libs import swapi_client
 import msvcrt
+import random
+
 init()
 
 
@@ -15,7 +17,6 @@ class Player:
 
 
 class FightGame:
-
     DEFAULT_LIVES = 2
     DEFAULT_POWER = 10
 
@@ -38,7 +39,7 @@ class FightGame:
 
         while True:
             option = msvcrt.getch()
-            print(option)
+            # print(option)
 
             if option == b'\x1b':
                 break
@@ -50,7 +51,7 @@ class FightGame:
                 self.__status()
             elif option == b'3':
                 self.__fight()
-            
+
     def __menu(self):
         print("\n\nAyuda:\n")
         print("0. Mostrar ayuda")
@@ -60,14 +61,84 @@ class FightGame:
         print("Esc. Salir\n")
 
     def __add_player(self):
-        print('add player')
+        name = ''
+        while len(name) < 1:
+            name = input('\n\nEscribe nombre del jugador: ')
+        gender = ''
+        while len(gender) < 1:
+            gender = input('\nElige sexo: (male | female): ')
+
+        self.last_player_id += 1
+
+        player = Player(
+            player_id=self.last_player_id,
+            name=name,
+            gender=gender,
+            power=FightGame.DEFAULT_POWER,
+            lives=FightGame.DEFAULT_LIVES
+        )
+        self.players.append(player)
+        print(Fore.YELLOW + '{} ha sido añadido'.format(name) + Fore.WHITE)
 
     def __status(self):
-        for p in self.players:
-            print(p.player_id, p.name, p.gender, p.lives, p.power, p.gems)
+        print(Fore.WHITE + 'Id'.ljust(4), 'Nombre'.ljust(21), 'Sexo'.ljust(9), 'Vidas'.ljust(6), 'Power'.ljust(6),
+              'Gemas')
+        print('----------------------------------------------------------------------------')
+        sorted_players = sorted(self.players, key=lambda x: x.power, reverse=True)
+        sorted_players = sorted(sorted_players, key=lambda x: x.lives, reverse=True)
+        for p in sorted_players:
+            color = Fore.RED if p.lives <= 0 else Fore.WHITE
+            print(color + '{}{}{}{}{}{}'.format(
+                '{}.'.format(p.player_id).ljust(5),
+                p.name.ljust(22),
+                p.gender.ljust(10),
+                str(p.lives).ljust(7),
+                str(p.power).ljust(7),
+                str(p.gems).ljust(7)))
 
     def __fight(self):
-        print('fight')
+        current_players = [x for x in self.players if x.lives > 0]
+
+        # hay más de un jugador?
+        if len(current_players) < 2:
+            print(Fore.RED + 'No hay suficientes jugadores')
+            return
+
+        fighters = random.sample(current_players, k=2)
+        player1 = fighters[0]
+        player2 = fighters[1]
+        damage = random.randint(1, 6)
+        player2.power -= damage
+        print(Fore.CYAN + '==> {} ha zurrado a {}'.format(player1.name, player2.name) + Fore.WHITE)
+
+        if player2.power <= 0:
+            player2.lives -= 1
+            player2.power = FightGame.DEFAULT_POWER if player2.lives > 0 else 0
+
+            if player2.lives > 0:
+                print(Fore.YELLOW + '{} ha perdido una vida'.format(player2.name) + Fore.WHITE)
+            else:
+                print(Fore.RED + '{} ha muerto'.format(player2.name) + Fore.WHITE)
+
+            player1.gems += 1
+            print(Fore.LIGHTGREEN_EX + '{} ha ganado una gema. Ahora tiene {} en total'.format(player1.name,
+                                                                                               player1.gems) + Fore.WHITE)
+
+            # cada 3 gemas le damos una vida
+            if player1.gems == 3:
+                player1.lives += 1
+                player1.gems = 0
+                print(Fore.MAGENTA + '{} ha ganado una vida!'.format(player1.name) + Fore.WHITE)
+
+            # comprobar si hay ganador (un solo jugador con vida)
+            if len([x for x in self.players if x.lives > 0]) == 1:
+                print("\n\n+============================================+");
+                print("+============================================+");
+                print("+============================================+");
+                print("      {} HA GANADO".format(player1.name));
+                print("+============================================+");
+                print("+============================================+");
+                print("+============================================+");
 
     def __get_players(self):
         print('Obteniendo jugadores desde la api de star wars...')
@@ -75,14 +146,15 @@ class FightGame:
         for person in people:
             self.last_player_id += 1
             player = Player(
-                player_id = self.last_player_id,
-                name = person[0],
-                gender = person[1],
-                power = FightGame.DEFAULT_POWER,
-                lives = FightGame.DEFAULT_LIVES
+                player_id=self.last_player_id,
+                name=person[0],
+                gender=person[1],
+                power=FightGame.DEFAULT_POWER,
+                lives=FightGame.DEFAULT_LIVES
             )
             self.players.append(player)
         print('Lista de jugadores preparada!')
         self.__status()
+
 
 FightGame('Diego').run()
